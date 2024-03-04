dcf31580415059a8605eb2a0734ccadfd4950cbb
Dalton <daltonfiggdavies@gmail.com>
2024-03-04 18:49:47 +0000

    :lipstick: Add styling for arrow button component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { FaArrowRightToBracket } from "react-icons/fa6";
 
 export default function Home() {
   return (
     <main className="grid">
-      <section className="z-50 col-start-1 col-end-1 row-start-1 row-end-1 m-10 self-end text-white">
+      <section className="z-50 col-start-1 col-end-1 row-start-1 row-end-1 m-10 grid grid-cols-3 items-end self-end text-white">
         <h1 className="text-8xl">12:30</h1>
+        <button className="mx-auto grid w-full max-w-[100px] place-items-center rounded-full border border-white/10 bg-white/10 py-3 backdrop-blur-sm transition hover:border-white/30 hover:bg-white/20">
+          <FaArrowRightToBracket />
+        </button>
       </section>
 
       <div
