d030bf7a219eb349b1d319435d750c5f0b8addd5
Dalton <daltonfiggdavies@gmail.com>
2024-03-06 17:46:38 +0000

    :hammer: Update configuration files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        rubik: "Rubik",
+        rubik: ['"Rubik"', ...defaultTheme.fontFamily.sans],
       },
     },
   },
