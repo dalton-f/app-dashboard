56864c3aeed281264d72daafdb15517a094d1dcf
Dalton <daltonfiggdavies@gmail.com>
2024-08-09 19:04:57 +0100

    Update pre-commit checks

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["dist/index.html", "./src/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "mountain-landscape":
+          "url('https://images.unsplash.com/photo-1483728642387-6c3bdd6c93e5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2076&q=80')",
+      },
+    },
   },
   plugins: [],
 };
