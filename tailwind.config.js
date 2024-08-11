259e3324289f10b6bcfb875bea13ae8e9afddf44
Dalton <daltonfiggdavies@gmail.com>
2024-08-11 08:35:19 +0100

    Logged out state visuals

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
   content: ["dist/index.html", "./src/**/*.js"],
   theme: {
@@ -6,6 +9,10 @@ export default {
       backgroundImage: {
         "mountain-landscape":
           "url('https://images.unsplash.com/photo-1483728642387-6c3bdd6c93e5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2076&q=80')",
+      },
+
+      fontFamily: {
+        rubik: ['"Rubik"', ...defaultTheme.fontFamily.sans],
       },
     },
   },
